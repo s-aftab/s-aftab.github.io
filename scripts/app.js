4443e6249b7e3d933e81866ce3127e3f4af252a7
Shehryar Aftab <shehryar.aftab@dcmail.ca>
2023-01-20 15:26:02 -0500

    Change Final - Hopefully

--- scripts/app.js
+++ scripts/app.js
@@ -3,12 +3,36 @@
 (function (){
 
 
+
     function DisplayHomePage(){
         let AboutUsButton = document.getElementById("AboutUsBtn");
         AboutUsButton.addEventListener("click", function(){
             location.href = "about.html"
         });
     }
+
+
+    let MainContent = document.getElementsByTagName("main")[0];
+    let MainParagraph = document.createElement("p");
+    MainParagraph.setAttribute("id", "MainParagraph");
+    MainParagraph.setAttribute("class", "mt-3");
+
+    /*
+    MainParagraph.textContent = "This is the Main Paragraph!";
+    MainContent.appendChild(MainParagraph);
+    */
+    let FirstString = "This is";
+    let SecondString = `${FirstString} the Main Paragraph`;
+    MainParagraph.textContent = SecondString;
+    MainContent.appendChild(MainParagraph);
+
+    let DocumentBody = document.getElementsByTagName("body")[0];
+    let Article = document.createElement("article");
+    let ArticleParagraph = `<p id="ArticleParagraph class="mt-3">This is my article paragraph</p>`;
+    Article.setAttribute("class","container");
+    Article.innerHTML = ArticleParagraph;
+    DocumentBody.appendChild(Article);
+
 
     function DisplayProductsPage(){
 
