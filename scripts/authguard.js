f85469713778c6e5f95578c62bd5d512bd97724f
Shehryar Aftab <shehryar.aftab@dcmail.ca>
2023-03-10 16:24:07 -0500

    ICE 4

--- scripts/authguard.js
+++ scripts/authguard.js
@@ -1,13 +1,10 @@
 "use strict";
-
-( function(){
-
-    if(!sessionStorage.getItem("user")){
-        location.href = "login.html";
+(function () {
+    let protected_routes = ["contact-list"];
+    if (protected_routes.indexOf(router.ActiveLink) > -1) {
+        if (!sessionStorage.getItem("user")) {
+            location.href = "/login";
+        }
     }
-
-
-
-
-
 })();
+//# sourceMappingURL=authguard.js.map
--- scripts/user.ts
+++ scripts/user.ts
@@ -1,72 +1,93 @@
 "use strict";
-var core;
-(function (core) {
-    class User {
-        m_displayName;
-        m_emailAddress;
-        m_username;
-        m_password;
-        constructor(displayName = "", emailAddress = "", username = "", password = "") {
+
+namespace core {
+
+
+    export  class User {
+        private m_displayName: string;
+        private m_emailAddress: string;
+        private m_username: string;
+        private m_password: string;
+        constructor(displayName:string = "", emailAddress:string = "", username:string = "", password:string = "") {
             this.m_displayName = displayName;
             this.m_emailAddress = emailAddress;
             this.m_username = username;
             this.m_password = password;
         }
-        get DisplayName() {
+
+        //getters
+        public  get DisplayName():string {
             return this.m_displayName;
         }
-        get EmailAddress() {
+
+
+        public get EmailAddress():string {
             return this.m_emailAddress;
         }
-        get Username() {
+
+        public get Username() :string{
             return this.m_username;
         }
-        get Password() {
+
+        public get Password():string {
             return this.m_password;
         }
-        set DisplayName(displayName) {
+
+        //setters
+        public set DisplayName(displayName:string) {
             this.m_displayName = displayName;
         }
-        set EmailAddress(emailAddress) {
+
+
+        public set EmailAddress(emailAddress: string) {
             this.m_emailAddress = emailAddress;
         }
-        set Username(username) {
+
+        public set Username(username:string) {
             this.m_username = username;
         }
-        set Password(password) {
+
+        public set Password(password:string) {
             this.m_password = password;
         }
-        toString() {
+
+        public  toString() : string{
             return `DisplayName: ${this.DisplayName}\n Email Address: ${this.EmailAddress} \n Username: ${this.Username}`;
         }
-        toJSON() {
+
+        public toJSON(): { Username: string, DisplayName: string, EmailAddress: string}{
             return {
                 "DisplayName": this.m_displayName,
                 "EmailAddress": this.m_emailAddress,
                 "Username": this.m_username
-            };
+
+            }
         }
-        fromJSON(data) {
+
+        fromJSON(data: User) {
             this.m_displayName = data.DisplayName;
             this.m_emailAddress = data.EmailAddress;
             this.m_username = data.Username;
             this.m_password = data.Password;
         }
-        serialize() {
+
+        public serialize(): string | null {
             if (this.DisplayName !== "" && this.Username !== "" && this.EmailAddress !== "" && this.m_password !== "") {
                 return `${this.DisplayName}, ${this.EmailAddress},${this.Username}, ${this.Password}`;
+
             }
             console.error("One or more of the properties of the contact object are missing or inavlid");
             return null;
         }
-        deserialize(data) {
+
+        public deserialize(data: string) {
             let propertyArray = data.split(",");
             this.m_displayName = propertyArray[0];
             this.m_emailAddress = propertyArray[1];
             this.m_username = propertyArray[2];
-            this.m_password = propertyArray[3];
+            this.m_password = propertyArray [3];
         }
+
     }
-    core.User = User;
-})(core || (core = {}));
-//# sourceMappingURL=user.js.map
+}
+
